97175275e1a3908b03400fe8512e5d2e29f6e0c3
Imad Shah <shahimad499@gmail.com>
2024-12-25 11:37:36 +0500

    feat: add colors and routes

---
+++ src/constants/color.ts
@@ -0,0 +1,27 @@
+export const COLORS = {
+  primary: {
+    main: "#DB4444",
+    lightBrown: "#E2B93B",
+    lightestGreen: "#F2FFCB",
+  },
+
+  secondary: {
+    main: "#47B486",
+    darkGreen: "#707428",
+  },
+
+  gray: {
+    main: "#7D8184",
+    darkGray: "#F5F5F5",
+    lightGray: "#FEFAF1",
+  },
+
+  white: {
+    main: "#FFFFFF",
+  },
+
+  dark: {
+    main: "#000000",
+    lightDark: "#363738",
+  },
+};
--- src/core/core.ts
+++ src/core/core.ts
@@ -1,1 +1,1 @@
-export {};
+export * from './Routes/Routes';
