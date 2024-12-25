84b2ae9e54292a51d5db03d90b7d7ff133b49b41
Imad Shah <shahimad499@gmail.com>
2024-12-25 16:21:51 +0500

    feat: add modules and screens

---
+++ src/modules/ManageAbout/container/AboutContainer/AboutContainer.tsx
@@ -0,0 +1,11 @@
+import { AppLayout } from '@muc/layout'
+
+const AboutContainer = () => {
+  return (
+    <AppLayout>
+       <h1>Aultjl containet</h1>
+    </AppLayout>
+  )
+}
+
+export default AboutContainer
---
+++ src/modules/ManageHome/container/HomeContainer/HomeContainer.tsx
@@ -0,0 +1,11 @@
+import { AppLayout } from "@muc/layout";
+
+const HomeContainer = () => {
+  return (
+    <AppLayout>
+      <h1>HOme Container</h1>
+    </AppLayout>
+  );
+};
+
+export default HomeContainer;
