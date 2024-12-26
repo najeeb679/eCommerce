79b79fe990f0f66815a20eb860c7eecb21b01b4b
Imad Shah <shahimad499@gmail.com>
2024-12-26 15:28:32 +0500

    fix: debugging

--- src/core/App/App.tsx
+++ src/core/App/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@emotion/react";
-import { theme } from "@muc/styles";
 import Routes from "../Routes/Routes";
+import { theme } from "@muc/styles";
 
 const App = () => {
   return (
