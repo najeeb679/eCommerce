203f36d866f79a866c6d2cc2941b73bd69952b4b
Imad Shah <shahimad499@gmail.com>
2024-12-27 14:29:22 +0500

    feat: responsive and code modifications

--- src/modules/ManageHome/components/TopSellingProduct/TopSellingProduct.tsx
+++ src/modules/ManageHome/components/TopSellingProduct/TopSellingProduct.tsx
@@ -13,7 +13,12 @@ const TopSellingProduct = () => {
           mb: 4,
         }}
       >
-        <Typography variant="h1">Best Selling Products</Typography>
+        <Typography
+          variant="h1"
+          sx={{fontSize:{md:'30px',sm:'auto',xs:'25px'}}}
+        >
+          Best Selling Products
+        </Typography>
         <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
           <CustomBtn
             title="view all"
@@ -23,7 +28,15 @@ const TopSellingProduct = () => {
           />
         </Box>
       </Box>
-      <Box sx={{ display: "flex", gap: "30px" }}>
+      <Box
+        sx={{
+          display: { md: "flex", sm: "flex", xs: "block" },
+          gap: "30px",
+          width: { md: "100%", sm: "100%", xs: "100%" },
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
         {TOP_SELLING_PRODUCTS.slice(0, 4).map((data) => (
           <ProductCard data={data} topProduct={true} />
         ))}
--- src/modules/ManageHome/container/HomeContainer/HomeContainer.tsx
+++ src/modules/ManageHome/container/HomeContainer/HomeContainer.tsx
@@ -1,20 +1,34 @@
 import { AppLayout } from "@muc/layout";
 import {
   CardSlider,
+  NewArrivalCard,
   TopSellingProduct,
   VoucherSlider,
 } from "../../components/components";
-import { BrowseByCategory, MusicExperience, OurFeature } from "@muc/components";
+import {
+  AsideLinkMenu,
+  BrowseByCategory,
+  MusicExperience,
+  OurFeature,
+} from "@muc/components";
 import { EXPLORE_OUR_PRODUCTS, PRODUCT_CARD } from "@muc/constants";
+import { Box } from "@mui/material";
 
 const HomeContainer = () => {
   return (
     <AppLayout>
-      <VoucherSlider />
-      <br/>
-      <br/>
-      <br/>
-      <br/>
+      <Box
+        sx={{
+          display: { md: "flex", sm: "block", xs: "block" },
+          gap: { md: "20px", sm: "10px", xs: 0 },
+          width: "100%",
+          my: { md: 6, sm: 4, xs: 0 },
+        }}
+      >
+        <AsideLinkMenu />
+        <VoucherSlider />
+      </Box>
+      <br />
       <OurFeature title="Today's" />
       <CardSlider title="Flash Sales" data={PRODUCT_CARD} />
       <OurFeature title="Categories" />
@@ -29,6 +43,9 @@ const HomeContainer = () => {
         data={EXPLORE_OUR_PRODUCTS}
       />
       <CardSlider explore={true} title={""} data={EXPLORE_OUR_PRODUCTS} />
+      <OurFeature title="Featured" />
+      <NewArrivalCard />
+      <br />
     </AppLayout>
   );
 };
