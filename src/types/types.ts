24f56ff003064e337dc061a22e455a2aa61678d2
Imad Shah <shahimad499@gmail.com>
2024-12-27 14:54:04 +0500

    feat: add define the types

--- src/types/types.ts
+++ src/types/types.ts
@@ -6,3 +6,26 @@ export type productCardType = {
   oldPrice: number;
   ratingValue: number;
 };
+
+export type categoryTypes={
+  img: string;
+  title: string;
+}
+
+export type topSellingProducts={
+  id: number;
+  poster: string;
+  title: string;
+  newPrice: number;
+  oldPrice: number;
+  ratingValue: number;
+}
+
+export type voucherTypes={
+  id: number;
+    categories: string;
+    categoryDescription: string;
+    title: string;
+    poster: string;
+    icons: string;
+}
